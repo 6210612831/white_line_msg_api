8eaf2b2e95d0218c2e69837dced8f35b67f61703
Pattana Sapphanachai <6210612831@student.tu.ac.th>
2022-06-20 09:55:17 +0700

    Create models db

--- message_api/admin.py
+++ message_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from message_api.models import Account, Audience
+from .views import *
+
 # Register your models here.
+admin.site.register(Audience)
+admin.site.register(Account)
--- message_api/models.py
+++ message_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Audience(models.Model):
+    name = models.CharField(max_length = 180)
+
+    def __str__(self):
+        return f"ID : {self.id} , NAME : {self.name} "
+
+
+class Account(models.Model):
+    user_id = models.CharField(max_length = 180)
+    group = models.ForeignKey(Audience, on_delete = models.SET_NULL, blank = True, null = True)
+    is_admin = models.BooleanField(default=False)
+    def __str__(self):
+        return f"ID : {self.id} , USER_ID : {self.user_id} , GROUP : {self.group} , ADMIN : {self.is_admin}"
