ab001e4eb0a835885e88face84a258e7f10ec6b6
Pattana Sapphanachai <6210612831@student.tu.ac.th>
2022-06-20 16:21:22 +0700

    Add sent message to Function

--- message_api/views.py
+++ message_api/views.py
@@ -1,41 +1,19 @@
-from email.headerregistry import Group
+import requests
+import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status  
-import requests
-import os
-import sys
-import wsgiref.simple_server
-from argparse import ArgumentParser
-
-from builtins import bytes
-from linebot import (
-    LineBotApi, WebhookParser
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
-)
-from linebot.utils import PY3
 from django.shortcuts import render
-import json
 from message_api.models import Account, Audience
-
 from message_api.serializers import AccountSerializer,AudienceSerializer
 
 CHANNEL_SECRET = "2fd29402f72edea813df01ad10854262"
 CHANNEL_ACCESS_TOKEN = "JtUxiyw8orHxQEgjPiFWXrQsYEYxnnNiSrnAwhzoXAJznUZLr/WqO7iWb76m/dqR2WBdox+hjeCwUVn8ApuVrVz833kAzrtFETGXx8fou0XnGrcwM++A0S/AZX0ygUii02fHLZpnw5V0zd3zK67MQwdB04t89/1O/w1cDnyilFU="
-AUDIENCEGROUPID = 4735937759476
 
 
 # Create your views here.
 class MessageApiView(APIView):
-
-
     
-
     def get(self, request, *args, **kwargs):
          return Response("serializer.data", status=status.HTTP_200_OK)
   
@@ -171,6 +149,45 @@ class MessageApiView(APIView):
             print(Audience.objects.all())
             return "delete group name : "+name +" done!"
 
+        # sent to (group) hello world
+        def sent_message_to(message,user_id):
+            account = Account.objects.filter(user_id=user_id)
+            if len(account) == 0 or (not account[0].is_admin):
+                return "You not have permission to create group"
+
+            group_message = message[message.find("sent to ")+len("sent to "):]
+            group_name = group_message[:group_message.find(" ")]
+            message_to_sent = group_message[group_message.find(" ")+1:]
+
+            group = Audience.objects.filter(name=group_name)
+
+            if len(group) == 0:
+                return "No group name : "+group_name
+
+            user_to_sent_list = Account.objects.filter(group__in=[group[0]])
+            url = f'https://api.line.me/v2/bot/message/multicast'
+            headers = {'content-type': 'application/json',
+                        "Authorization" : f"Bearer {CHANNEL_ACCESS_TOKEN}"
+                }
+            payload = {
+                            "to": [f"{userId}"],
+                            "messages": 
+                                [
+                                    {
+                                        "type":"text",
+                                        "text": message_to_sent
+                                    }
+                                ]
+                    }
+   
+            for user in user_to_sent_list:
+                payload['to']=[f"{user.user_id}"]
+                res = requests.post(url, headers=headers,data=json.dumps(payload))
+                print("STATUS FOR PUT MESSAGE TO USER : ",res.status_code,res.json(),res.data)
+                print("MESSAGE TO USER : "+ message_to_sent)
+            return "Sent to group "+group[0].name+" done!!"
+
+
         def check_message(message,user_id):
             message = message.lower()
             if message.find("join group") != -1:
@@ -189,8 +206,9 @@ class MessageApiView(APIView):
                 return clear_user(user_id)
             elif message.find("deletegroup")!= -1:
                 return delete_group(message,user_id)
+            elif message.find("sent to")!= -1:
+                return sent_message_to(message,user_id)
             return message
-
 
         global CHANNEL_SECRET,CHANNEL_ACCESS_TOKEN
 
@@ -222,7 +240,6 @@ class MessageApiView(APIView):
         print("STATUS FOR PUT MESSAGE TO USER : ",res.status_code,res.json())
         print("MESSAGE TO USER : "+return_to_user_text)
         return Response("", status=status.HTTP_200_OK)
-
 
 
 def index(request):
